dfd82037db85b868382c36bc01fb9107fde4c722
davy1ex <davy1ex@yandex.ru>
2023-10-12 13:56:53 +0400

    initial commit

--- upasi/lending/forms.py
+++ upasi/lending/forms.py
@@ -5,4 +5,5 @@ class MyForm(forms.Form):
     name = forms.CharField(label='Имя', max_length=100)
     email = forms.EmailField(label='Адрес электронной почты')
     comment = forms.CharField(label='Примечание', max_length=100)
+    
 
--- upasi/lending/views.py
+++ upasi/lending/views.py
@@ -3,16 +3,34 @@ from .forms import MyForm
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    if request.method == 'POST':
+        form = MyForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            name = form.cleaned_data['name']
+            comment = form.cleaned_data['comment']
 
+            with open('contacts.txt', 'a') as file:
+                file.write(f'{name}, {email}, {comment}\n')
+
+            return render(request, 'main/index.html', {'form': form})
+    else:
+        form = MyForm()
+
+    return render(request, 'main/index.html', {'form': form})
 
 def create_form(request):
     if request.method == 'POST':
         form = MyForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
             email = form.cleaned_data['email']
+            name = form.cleaned_data['name']
             comment = form.cleaned_data['comment']
+
+            with open('contacts.txt', 'a') as file:
+                file.write(f'{name}, {email}, {comment}\n')
+
+            return redirect('success_url')  # замените 'success_url' на URL-адрес страницы успешной отправки
     else:
         form = MyForm()
 
